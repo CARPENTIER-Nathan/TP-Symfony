b4c77103ac4f6974aa47005cc206f657bbea1cbb
Nat <carpentier.nathan2704@gmail.com>
2022-11-30 10:53:22 +0100

    version 3

--- public/traduction.php
+++ public/traduction.php
@@ -1,17 +1,17 @@
 <?php
 
-use Symfony\Component\Translation\Loader\XliffFileLoader;
-use Symfony\Component\Translation\Translator;
+// use Symfony\Component\Translation\Loader\XliffFileLoader;
+// use Symfony\Component\Translation\Translator;
 
-require_once "../vendor/autoload.php";
+// require_once "../vendor/autoload.php";
 
-$translator = new Translator('fr');
-$translator->addLoader('xlf', new XliffFileLoader());
-$translator->addResource(
-    'xlf',
-    __DIR__.'/../translations/validators.fr.xlf',
-    'fr'
-);
+// $translator = new Translator('fr');
+// $translator->addLoader('xlf', new XliffFileLoader());
+// $translator->addResource(
+//     'xlf',
+//     __DIR__.'/../translations/validators.fr.xlf',
+//     'fr'
+// );
 
-echo $translator->trans("One case can't be blank.");
+// echo $translator->trans("One case can't be blank.");
 ?>
